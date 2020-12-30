f09401fe3dc3de5d32f8fac5875c9a204a204b9d
chrisjonesBSU <chrisjones4@u.bosiestate.edu>
2020-12-29 21:53:21 -0700

    added conversion factors for kcal, kj and j

--- uli_init/utils/base_units.py
+++ uli_init/utils/base_units.py
@@ -3,6 +3,9 @@ def base_units():
     units["avogadro"] = 6.022140857e23
     units["boltzmann"] = 1.38064852e-23
     units["kj_to_j"] = 1e3
+    units["kj_to_kcal"] = 0.2390057
+    units["kcal_to_kj"] = 4.184
+    units["kcal_to_j"] = 4.184e3
     units["amu_to_kg"] = 1.6605e-27
     units["amu_to_g"] = 1.6605e-24
     units['cm_to_nm'] = 1e7
